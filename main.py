4d00d4e820fc0b6964e0077a76ae95b290c5264d
murilo <nerdoswampo@gmail.com>
2021-10-25 16:08:40 -0300

    [Tried polishing the code a bit]

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from discord.ext import commands, tasks
 from itertools import cycle
 from replit import db
 from keep_alive import keep_alive
-from test import mafs, operation, power, factorial
+from mathops import mafs, operation, power, factorial
 from sussifier import sussy
+from weather import weather
 
 """
 https://pythonrepo.com/repo/nextcord-nextcord << u
@@ -247,26 +248,8 @@ async def on_message(message):
 # COMANDO DE CLIMA =WEATHER
   if message.content.startswith("=weather"):
     city = (message.content.split("=weather ",1)[1])
-    Wclient = python_weather.Client(format=python_weather.IMPERIAL)
-    weather = await Wclient.find(city)
-    temper = weather.current.temperature
-    conditions = weather.current.sky_text
-    if temper <= 15:
-      imgs = [' https://media.discordapp.net/attachments/818907430560989264/859856960026968084/a1a2b48d-0648-4d6f-8952-41b46ad9a68e.png?width=545&height=676', ' https://cdn.discordapp.com/attachments/818907430560989264/859867533296795678/282b0274-d21a-4cbb-85b2-c65783c2665c.png']
-      context = random.choice(imgs)
-      await message.channel.send(f'It is {conditions} in {city}, currently {temper} ºC')
-      time.sleep(0.1)
-      await message.channel.send(context)
-    elif temper >= 25:
-      context = ' https://staticr1.blastingcdn.com/media/photogallery/2016/10/21/660x290/b_1200x680/a-internet-nao-perdoou-o-calor-da-ultima-semana_933227.jpg'
-      await message.channel.send(f'It is {conditions} in {city}, currently {temper} ºC')
-      time.sleep(0.1)
-      await message.channel.send(context)
-    else:
-      await message.channel.send(f'It is {conditions} in {city}, currently {temper} ºC')
-      time.sleep(0.1)
-      await message.channel.send('https://thumbs.dreamstime.com/z/bald-man-thumbs-up-26364248.jpg')
-    await Wclient.close()
+    msg = message
+    weather(city, msg)
 
 # RETORNAR COR
   if message.content.startswith("=color"):
