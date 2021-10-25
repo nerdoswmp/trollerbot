c778ff44279b7ed90d18f32752ce03e5f6a64a6d
murilo <nerdoswampo@gmail.com>
2021-10-25 15:51:31 -0300

    [cool grafs]

--- main.py
+++ main.py
@@ -352,8 +352,6 @@ async def on_message(message):
 keep_alive()
 client.run(os.getenv('deeznuts'))
 
-# test
-# test 2
 # filler comment on a different branch check this out
 # adding this comment just so i cna have a cool looking network graph
   # ^^ awesome ive merged both of them
